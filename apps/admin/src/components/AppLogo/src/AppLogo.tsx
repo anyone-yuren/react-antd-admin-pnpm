46aadbae3fe694f51dd081bffd14f5437e7d0d58
G <shoplazza.nyg@gmail.com>
2023-12-18 15:32:30 +0800

    feat: :sparkles: 修改logo、系统名称样式

--- apps/admin/src/components/AppLogo/src/AppLogo.tsx
+++ apps/admin/src/components/AppLogo/src/AppLogo.tsx
@@ -1,4 +1,5 @@
 import { Space } from 'antd';
+import { useTheme } from 'antd-style';
 import classNames from 'classnames';
 
 import SvgIcon from '@/components/SvgIcon';
@@ -11,14 +12,25 @@ import useStyles from './app-logo.module.style';
 import type { FC } from 'react';
 
 const AppLogo: FC = () => {
+  const token = useTheme();
   const { styles } = useStyles();
   const getMenuFold = useAppSelector((state) => state.app.appConfig?.menuSetting?.menuFold);
 
   return (
     <div className={classNames('anticon', styles['app-logo'])}>
       <Space>
-        <SvgIcon name='logo' size={30} />
-        <img className={classNames(styles['logo-name'], { [styles.hidden]: getMenuFold })} src={logoName} alt='logo' />
+        <SvgIcon
+          style={{
+            color: token.colorPrimary,
+          }}
+          name='logo'
+          size={30}
+        />
+        <div className={styles.container}>
+          <span className={styles.text}>{'Beata Admin'}</span>
+        </div>
+
+        {/* <img className={classNames(styles['logo-name'], { [styles.hidden]: getMenuFold })} src={logoName} alt='logo' /> */}
       </Space>
     </div>
   );
