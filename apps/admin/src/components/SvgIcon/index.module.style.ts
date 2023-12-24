3b1fa1074a8910908e517d58b72b18a9908dd0d3
G <shoplazza.nyg@gmail.com>
2023-12-24 23:21:22 +0800

    feat: :sparkles: 去除多余样式

--- apps/admin/src/components/SvgIcon/index.module.style.ts
+++ apps/admin/src/components/SvgIcon/index.module.style.ts
@@ -1,13 +1,11 @@
 import { createStyles } from 'antd-style';
 
-const useStyles = createStyles(() => {
-  return {
-    'svg-icon': {
-      display: 'inline-block',
-      verticalAlign: '-0.15em',
-      fill: 'currentColor',
-      overflow: 'hidden',
-    },
-  };
-});
+const useStyles = createStyles(() => ({
+  'svg-icon': {
+    display: 'inline-block',
+    verticalAlign: '-0.15em',
+    fill: 'currentColor',
+    overflow: 'hidden',
+  },
+}));
 export default useStyles;
