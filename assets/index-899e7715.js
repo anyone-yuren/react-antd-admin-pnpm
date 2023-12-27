71f14f00d4d69aed62b45f8f55e74a6710356e01
anyone-yuren <1003473088@qq.com>
2023-12-27 07:20:41 +0000

    Deploying to gh-pages from @ anyone-yuren/react-antd-admin-pnpm@8a341677fc480bff55ec4b9c8ba8d1a6eaa1bedc 🚀

---
+++ assets/index-899e7715.js
@@ -0,0 +1,1 @@
+import{j as d}from"./index-4e54c261.js";const o=({onAdd:o=(()=>{})})=>(console.log(o),d.jsx("div",{children:d.jsx("div",{children:"添加用户"})}));export{o as default};
--- assets/index-99b3749c.js
+++
@@ -1,1 +0,0 @@
-import{j as d}from"./index-9076b4a1.js";const o=({onAdd:o=(()=>{})})=>(console.log(o),d.jsx("div",{children:d.jsx("div",{children:"添加用户"})}));export{o as default};
