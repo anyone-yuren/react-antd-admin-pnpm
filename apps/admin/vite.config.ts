3bbec41a96462e42bfa5794a768148be3f072ea4
G <shoplazza.nyg@gmail.com>
2024-01-02 15:43:43 +0800

    feat: :sparkles: test

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -24,7 +24,7 @@ export default ({ command, mode }: ConfigEnv): UserConfig => {
   const { VITE_PORT, VITE_DROP_CONSOLE } = viteEnv;
 
   return {
-    base: './',
+    base: isBuild ? '/react-antd-admin-pnpm/' : './',
     server: {
       // Listening on all local ips
       host: true,
