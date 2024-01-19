14f5a285451a02eae30360b1e0575d99fae40ee8
G <shoplazza.nyg@gmail.com>
2024-01-19 11:14:54 +0800

    ci: :ferris_wheel: 打包文档到docs仓库

--- apps/admin-docs/.dumirc.ts
+++ apps/admin-docs/.dumirc.ts
@@ -4,4 +4,8 @@ export default defineConfig({
   themeConfig: {
     name: '@gbeata/admin-docs',
   },
+  publicPath:
+    process.env.NODE_ENV === 'production' ? '/gbeata-admin-docs/' : '/',
+  outputPath: '../../docs-dist',
+  base: process.env.NODE_ENV === 'production' ? '/gbeata-admin-docs/' : '/',
 });
