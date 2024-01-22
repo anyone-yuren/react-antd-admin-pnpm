dab0a5a24d9048c63485a1d53bdbb5ebdcb5a7b3
G <shoplazza.nyg@gmail.com>
2024-01-22 09:55:56 +0800

    feat: :sparkles: 补充项目文档地址

--- apps/admin/src/layout/feature/components/DocLink.tsx
+++ apps/admin/src/layout/feature/components/DocLink.tsx
@@ -1,10 +1,17 @@
 import { ReadOutlined } from '@ant-design/icons';
 import { Button, Tooltip } from 'antd';
 
+import { openWindow } from '@/utils';
+
 export default function DocLink() {
   return (
     <Tooltip title='文档' placement='bottom' mouseEnterDelay={0.5}>
-      <Button shape='circle' size='small' icon={<ReadOutlined />} />
+      <Button
+        shape='circle'
+        size='small'
+        onClick={() => openWindow('https://anyone-yuren.github.io/gbeata-admin-docs')}
+        icon={<ReadOutlined />}
+      />
     </Tooltip>
   );
 }
