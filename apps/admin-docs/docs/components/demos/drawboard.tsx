19efaa88648a56fa8eed1650c72cbe60d2003463
choikeith <1278028801@qq.com>
2024-02-21 17:30:07 +0800

    feat(ui): drawboard component

---
+++ apps/admin-docs/docs/components/demos/drawboard.tsx
@@ -0,0 +1,11 @@
+import { DrawBoard } from 'ui';
+import { Center } from 'react-layout-kit';
+import React from 'react';
+
+export default () => {
+  return (
+    <>
+      <DrawBoard stroke='#df4b26' strokeWidth={5} />
+    </>
+  );
+};
