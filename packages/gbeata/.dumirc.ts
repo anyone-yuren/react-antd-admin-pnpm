953a9fef76bcef1f70f68f357d68e3036b7d7f53
G <shoplazza.nyg@gmail.com>
2024-05-14 23:39:19 +0800

    build(gbeata): :package: add gbeata component build command

--- packages/gbeata/.dumirc.ts
+++ packages/gbeata/.dumirc.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
-  outputPath: 'docs-dist',
+  outputPath: '../../gbeata-dist',
   themeConfig: {
     name: 'gbeata',
     mfsu: false,
