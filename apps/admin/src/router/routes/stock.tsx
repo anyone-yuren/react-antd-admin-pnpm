9b3e34816dd4fd227abf446c4f1cc30b8d8137fd
G <shoplazza.nyg@gmail.com>
2024-08-16 16:12:47 +0800

    feat: :sparkles: 新增现存量

---
+++ apps/admin/src/router/routes/stock.tsx
@@ -0,0 +1,33 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { lazy } from '@loadable/component';
+import { t } from 'i18next';
+import { useTranslation } from 'react-i18next';
+
+import LazyLoad from '@/components/LazyLoad';
+
+import { LayoutGuard } from '../guard';
+
+import type { RouteObject } from '../types';
+
+// user module page
+const UserRoute: RouteObject = {
+  path: '/stock',
+  meta: {
+    title: '现存量',
+    icon: 'gallery',
+    orderNo: 3,
+    iconSize: 20,
+    hideChildrenInMenu: true,
+  },
+  children: [
+    {
+      path: '',
+      element: LazyLoad(lazy(() => import('@/views/stock'))),
+      meta: {
+        title: '',
+        key: '',
+      },
+    },
+  ],
+};
+export default UserRoute;
