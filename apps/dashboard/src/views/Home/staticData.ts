5268b58f97d84d6998361b14152355c69c820e5c
G <shoplazza.nyg@gmail.com>
2025-01-06 11:48:11 +0800

    feat: 静态数据

---
+++ apps/dashboard/src/views/Home/staticData.ts
@@ -0,0 +1,128 @@
+// 库存金额
+export const supplierInventoryList = [
+  {
+    supplierName: '南京钢铁股份有限公司',
+    orderAmount: 21338703.543293,
+  },
+  {
+    supplierName: '平顶山市科睿新材料制造有限公司',
+    orderAmount: 11173212.032282002,
+  },
+  {
+    supplierName: '鞍钢钢绳有限责任公司',
+    orderAmount: 9957457.17075,
+  },
+  {
+    supplierName: '中国平煤神马控股集团有限公司招标采购中心',
+    orderAmount: 6936857.141801,
+  },
+  {
+    supplierName: '洛阳金鹭硬质合金工具有限公司',
+    orderAmount: 5312797.879,
+  },
+  {
+    supplierName: '贵州钢绳股份有限公司郑州销售分公司',
+    orderAmount: 5405923.097476,
+  },
+  {
+    supplierName: '衡阳华菱钢管有限公司',
+    orderAmount: 4867935.784378,
+  },
+  {
+    supplierName: '平顶山天成矿山工程设备有限公司',
+    orderAmount: 3599220.13877,
+  },
+  {
+    supplierName: '宁夏恒力钢丝绳有限公司',
+    orderAmount: 4313547.002383,
+  },
+  {
+    supplierName: '平顶山市宏泽物资有限公司',
+    orderAmount: 4293764.898106,
+  },
+];
+export const supplierSummaryList = [
+  {
+    supplierName: '河南天工科技股份有限公司',
+    orderAmount: 53728196.572742,
+  },
+  {
+    supplierName: '平顶山天成矿山工程设备有限公司',
+    orderAmount: 52392745.950163,
+  },
+  {
+    supplierName: '平顶山市科睿新材料制造有限公司',
+    orderAmount: 48224823.360636,
+  },
+  {
+    supplierName: '平顶山泰克斯特高级润滑油有限公司',
+    orderAmount: 29948091.94533818,
+  },
+  {
+    supplierName: '南京钢铁股份有限公司',
+    orderAmount: 28743149.260792,
+  },
+  {
+    supplierName: '洛阳金鹭硬质合金工具有限公司',
+    orderAmount: 26398962.344,
+  },
+  {
+    supplierName: '平煤神马机械装备集团河南重机有限公司',
+    orderAmount: 23934231.982687,
+  },
+  {
+    supplierName: '湖南华菱湘潭钢铁有限公司',
+    orderAmount: 20683234.23958,
+  },
+  {
+    supplierName: '平顶山市矿益胶管制品股份有限公司',
+    orderAmount: 19959209.456587,
+  },
+  {
+    supplierName: '河南奋达科技有限公司',
+    orderAmount: 19671063.59195,
+  },
+];
+
+export const invoiceSupplierSummaryList = [
+  {
+    supplierName: '河南天工科技股份有限公司',
+    orderAmount: 50440063.386342,
+  },
+  {
+    supplierName: '平顶山天成矿山工程设备有限公司',
+    orderAmount: 48793525.8113908,
+  },
+  {
+    supplierName: '平顶山市科睿新材料制造有限公司',
+    orderAmount: 37051611.328354,
+  },
+  {
+    supplierName: '平顶山泰克斯特高级润滑油有限公司',
+    orderAmount: 28921219.96545047,
+  },
+  {
+    supplierName: '洛阳金鹭硬质合金工具有限公司',
+    orderAmount: 21086164.464,
+  },
+  {
+    supplierName: '平煤神马机械装备集团河南重机有限公司',
+    orderAmount: 21007637.200827,
+  },
+  {
+    supplierName: '湖南华菱湘潭钢铁有限公司',
+    orderAmount: 20683234.23958,
+  },
+  {
+    supplierName: '平顶山市矿益胶管制品股份有限公司',
+    orderAmount: 18312236.794172,
+  },
+  {
+    supplierName: '河南奋达科技有限公司',
+    orderAmount: 17156625.106235,
+  },
+  {
+    supplierName: '郑州华克智能科技有限公司',
+    orderAmount: 15146695.888458,
+  },
+];
