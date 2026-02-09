d972ac304173a5ffc32ee3e4c954c23b833623a4
G <shoplazza.nyg@gmail.com>
2026-02-09 16:43:39 +0800

    feat: 修改logo

--- apps/admin/src/components/AppLogo/src/AppLogo.tsx
+++ apps/admin/src/components/AppLogo/src/AppLogo.tsx
@@ -4,6 +4,7 @@ import classNames from 'classnames';
 
 import SvgIcon from '@/components/SvgIcon';
 
+import logo1 from '@/assets/images/logo1.jpg';
 import { useSettings } from '@/stores/modules/settingStore';
 
 import useStyles from './app-logo.module.style';
@@ -19,14 +20,17 @@ const AppLogo: FC = () => {
   return (
     <div className={classNames('anticon', styles['app-logo'])}>
       <Space>
-        <SvgIcon
+        {/* <SvgIcon
           style={{
             color: token.colorPrimary,
           }}
           name='logo'
           size={30}
-        />
-        <div className={styles.container}>{!getMenuFold ? <span className={styles.text}>{'神马集团'}</span> : ''}</div>
+        /> */}
+        <img src={logo1} alt='logo' style={{ width: 30, height: 30 }} />
+        <div className={styles.container}>
+          {!getMenuFold ? <span className={styles.text}>{'中国平煤神马集团'}</span> : ''}
+        </div>
 
         {/* <img className={classNames(styles['logo-name'], { [styles.hidden]: getMenuFold })} src={logoName} alt='logo' /> */}
       </Space>
